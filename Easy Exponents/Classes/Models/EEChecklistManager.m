500733835bb5167b2f2f896c016f7a777cb596b5
Mitchell Porter <mitchellporter@gmail.com>
2014-09-04 19:21:41 -0700

    Edited save and fetch methods of checklist manager

--- Easy Exponents/Classes/Models/EEChecklistManager.m
+++ Easy Exponents/Classes/Models/EEChecklistManager.m
@@ -1,5 +1,6 @@
 
 #import "EEChecklistManager.h"
+#import "EEAppDelegate.h"
 
 #pragma mark - Static Variables
 static NSString *kSavedSwipedCellRows = @"savedSwipedCellRows";
@@ -22,12 +23,32 @@ static NSString *kSavedSwipedCellRows = @"savedSwipedCellRows";
 #pragma mark - Instance methods
 - (void)saveChecklist
 {
+
     if (!self.swipedCellIdNumbers) {
         self.swipedCellIdNumbers = [[NSMutableArray alloc]init];
     }
+    
+    for (NSNumber *swipedCellIdNumber in self.swipedCellIdNumbers) {
+        
+        EEAppDelegate *appDelegate =
+        [[UIApplication sharedApplication] delegate];
+        
+        NSManagedObjectContext *context =
+        [appDelegate managedObjectContext];
+        NSManagedObject *cellIdNumber;
+        
+        cellIdNumber = [NSEntityDescription
+                        insertNewObjectForEntityForName:@"Cell_Id_Numbers"
+                        inManagedObjectContext:context];
+        
+        [cellIdNumber setValue:swipedCellIdNumber forKey:@"id_number"];
+        
+        NSError *error;
+        [context save:&error];
+        
+    }
 
-    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
-    [standardDefaults setObject:[NSKeyedArchiver archivedDataWithRootObject:self.swipedCellIdNumbers] forKey:kSavedSwipedCellRows];
+
 }
 
 - (void)fetchSavedChecklist
@@ -36,9 +57,37 @@ static NSString *kSavedSwipedCellRows = @"savedSwipedCellRows";
         self.swipedCellIdNumbers = [[NSMutableArray alloc]init];
     }
     
-    NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
-    NSData *data = [standardDefaults objectForKey:kSavedSwipedCellRows];
-    self.swipedCellIdNumbers = [NSKeyedUnarchiver unarchiveObjectWithData:data];   
+    EEAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context =
+    [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entityDesc =
+    [NSEntityDescription entityForName:@"Cell_Id_Numbers"
+                inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDesc];
+    
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request
+                                              error:&error];
+    
+    if ([objects count] == 0) {
+        NSLog(@"No swiped cells persisted.");
+        
+    } else {
+        
+        //Lop through returned objects, create NSNumbers,
+        //and add NSNumbers to swipedCellRows
+        for (id object in objects) {
+            
+            NSManagedObject *managedObject = object;
+            NSNumber *finalCellIdNumber = [managedObject valueForKey:@"id_number"];
+            [self.swipedCellIdNumbers addObject:finalCellIdNumber];
+            
+        }
+    }
 }
 
 @end
